cc608fc6785b3a17f4e64f5029ce5ef4d33fdb5c
sezeristif <sezeristif@gmail.com>
2023-05-27 21:06:11 +0300

    add morgan logger

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 3000;
+const morgan = require("morgan");
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+app.use(morgan("combined")); // for logging
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
